5c3d702a9c0f932a3be3ed3ac6757ca7ee73ac31
franaj007 <najdek.franciszek@gmail.com>
2024-04-11 14:36:09 +0200

    11.04-working_user_input_no_html

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const ingredients = 'apples,flour,sugar';
-const maxRecipes = 2; // You can adjust this as needed
+let ingredients = '';
+const maxRecipes = 2; 
 const apiKey = '3611510279734103a44c257d8dfa11ab'
-const apiUrl = 
-`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=${maxRecipes}&apiKey=${apiKey}`;
 
-fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
+
+const btn = document.getElementById("btn");
+const userInput = document.getElementById("userInput");
+const out = document.getElementById("out");
+
+
+btn.addEventListener("click", function()
+{
+    ingredients = userInput.value;
+    let apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=${maxRecipes}&apiKey=${apiKey}`;
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
     // Handle the data (e.g., display recipe titles, images, etc.)
     console.log(data);
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
     console.error('Error fetching data:', error);
   });
+});
