dc774d0a891d741b4daef8fefb03ff34edb21e65
franaj007 <najdek.franciszek@gmail.com>
2024-04-15 11:40:56 +0200

    15.04-great_html_css_api_50/50

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 let ingredients = '';
-const maxRecipes = 2; 
+let titleMatcher = '';
+const maxRecipes = 1; 
 const apiKey = '3611510279734103a44c257d8dfa11ab'
 
 
-const btn = document.getElementById("btn");
-const userInput = document.getElementById("userInput");
-const out = document.getElementById("out");
+const btn1 = document.getElementById("btn1");
+const userInput1 = document.getElementById("userInput1");
+const output1 = document.getElementById("output1");
+
+const btn2 = document.getElementById("btn2");
+const userInput2 = document.getElementById("userInput2");
+const output2 = document.getElementById("output2");
 
 
-btn.addEventListener("click", function()
+btn1.addEventListener("click", function()
 {
-    ingredients = userInput.value;
+  output1.innerHTML = "";
+    ingredients = userInput1.value;
     let apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=${maxRecipes}&apiKey=${apiKey}`;
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+    // Handle the data (e.g., display recipe titles, images, etc.)
+    console.log(data);
+
+    output1.innerHTML =
+    "<h2>" + "Recipe:" + "</h2>" + data[0].title +
+      "<img src="+data[0].image+ " alt='recipe image'>";
+    
+  
+    })
+
+
+
+    .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
+  
+  
+
+});
+
+
+btn2.addEventListener("click", function()
+{
+  output2.innerHTML = " ";
+    titleMatcher = userInput2.value;
+    let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?titleMatch=${titleMatcher}&number=${maxRecipes}&apiKey=${apiKey}`;
 
     fetch(apiUrl)
     .then((response) => response.json())
@@ -22,4 +58,6 @@ btn.addEventListener("click", function()
     .catch((error) => {
     console.error('Error fetching data:', error);
   });
+
+
 });
