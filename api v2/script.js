d23ec59326cfcf9cd795d50143df54dce7ebcfb7
Rumeleq <mmikolaj0770@gmail.com>
2024-04-18 18:21:45 +0200

    Revert "18.04-imdyingslowlyinside"
    This reverts commit 17f5effb4c6bc32d8cdaad21b1278dc8f68be014.

--- api v2/script.js
+++ api v2/script.js
@@ -96,25 +96,14 @@ class Slider
         let fullRecipeURL = `https://api.spoonacular.com/recipes/1096282/analyzedInstructions?apiKey=${apiKey}`;
         let data = await fetch(fullRecipeURL);
         let fullRecipeSteps = await data.json();
-        let helpMeNiggaImDying = fullRecipeSteps;
         fullRecipeSteps = fullRecipeSteps[0].steps;
         console.log(fullRecipeSteps);
-        let helper1;
+        let helper;
         for(let i = 0; i < fullRecipeSteps.length; i++)
         {
-            helper1 = addElement("li", "#fullRecipeList");
-            helper1.innerText = fullRecipeSteps[i].step;
-            let helper2;
-            helper2 = addElement("li", "#ingredientsList");
-            let helper3 = helpMeNiggaImDying;
-            if(helper3[i].ingredients[0] != null)
-            {
-                for (let j = 0; j < helper3[i].ingredients.length; i++)
-                {
-                    helper2 = addElement("li", "#ingredientsList");
-                    helper2.innerText = helper3[i].ingredients[j];
-                }
-            }
+            helper = addElement("li", "#fullRecipeList");
+            helper.innerText = fullRecipeSteps[i].step;
+
 
         }
         $("document").ready(function()
@@ -123,7 +112,6 @@ class Slider
         })
 
     }
-
 
     updateRecipe = (value) =>
     {
@@ -183,16 +171,6 @@ byRecipeInput.focus();
 
 btn.addEventListener("click", function()
 {
-    const seeFullRecipeBtn = document.getElementById("seeFullRecipeBtn");
-    seeFullRecipeBtn.style.display = "inherit";
-    seeFullRecipeBtn.style.height = "auto";
-    seeFullRecipeBtn.style.width = "auto";
-
-    const leftNigga = document.getElementById("niggaLeft");
-    leftNigga.style.display = "inherit";
-    const rightNigga = document.getElementById("niggaRight");
-    rightNigga.style.display = "inherit";
-
     if (checkValue())
     {
         let apiUrl = getApiUrl();
